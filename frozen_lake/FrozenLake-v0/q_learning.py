f9cc1e65d684376848b2d1e70128c2bbd2df257e
Robert Meyer <meyerrobert@outlook.com>
2019-05-15 00:46:25 -0400

    adding q learning for frozen lake basic example - not complete

---
+++ frozen_lake/FrozenLake-v0/q_learning.py
@@ -0,0 +1,27 @@
+import gym
+import numpy as np
+
+env = gym.make('FrozenLake-v0')
+q_table = np.zeros(shape=(env.observation_space.n, env.action_space.n))
+
+learning_rate = 0.8
+discount_factor = 0.95
+epochs = 2000
+
+reward_list = []
+for run_number in range(epochs):
+    state = env.reset() # returns the initial observation space
+    total_reward = 0
+    task_complete = False
+    actions_taken = 0
+    while actions_taken < 99:
+        actions_taken += 1
+        action = np.argmax(q_table[state:] + np.random.randn(1, env.action_space.n) / (run_number + 1))
+        next_state, reward, task_complete, _ = env.step(action)
+        q_table[state, action] = q_table[state, action] + learning_rate * (reward + discount_factor * np.max(q_table[next_state, :]) - q_table[state, action])
+        total_reward += reward
+        state = next_state
+        if task_complete:
+            break
+    reward_list.append(total_reward)
+print(q_table)
